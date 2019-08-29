af0b803f8e694b5d88e04b34ee10f6e797b01dd3
pheilbro <pheilbro@student.42.us.org>
2019-08-29 15:29:04 -0700

    Update ft_ssl_md5.

--- inc/ft_ssl.h
+++ inc/ft_ssl.h
@@ -6,7 +6,7 @@
 /*   By: pheilbro <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 13:36:05 by pheilbro          #+#    #+#             */
-/*   Updated: 2019/08/29 11:47:00 by pheilbro         ###   ########.fr       */
+/*   Updated: 2019/08/29 15:27:48 by pheilbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include "ft_vector.h"
 # include "ft_dstring.h"
-# include "ft_ssl_error.h"
 # include "ft_ssl_md5.h"
 
 # define _P 1
@@ -23,15 +22,22 @@
 # define _R 4
 # define _S 8
 
+# define INV_COMMAND -1
+# define INV_OPTION -2
+# define INV_FILE -3
+# define DIRECTORY -4
+# define MISSING_ARG -5
+# define SYS_ERROR -6
+
 enum	e_ssl_algorithm
 {
-	md5,
+	md5 = 1,
 	sha256
 };
 
 enum	e_ssl_algorithm_type
 {
-	standard,
+	standard = 1,
 	message_digest,
 	cipher
 };
@@ -60,11 +66,23 @@ typedef struct	s_ssl_checksum
 	t_vector		*files;
 }				t_ssl_checksum;
 
+typedef struct	s_err_message
+{
+	int		errno;
+	char	*message;
+}				t_err_message;
+
+typedef struct	s_error
+{
+	int		no;
+	char	*data;
+}				t_error;
+
 t_ssl_algorithm	g_algo_tab[] =
 {
 	{md5, "md5", message_digest, &ft_ssl_md5},
 //	{sha256, "sha256", message_digest, &ft_ssl_sha256},
-	NULL
+	{0, NULL, 0, NULL}
 };
 
 void			ft_ssl_compute_checksum(t_ssl_checksum *chk);
@@ -85,5 +103,9 @@ void			parse_ssl_string(t_ssl_checksum *chk, char **data, int *i,
 		t_error *e);
 int				parse_ssl_file(t_ssl_checksum *chk, char **data, int i,
 		t_error *e);
+
+void			print_usage(t_ssl_checksum chk);
+void			print_fatal_error(t_error e, t_ssl_checksum chk);
+void			set_ssl_error(t_ssl_file *file, t_error e);
 
 #endif
